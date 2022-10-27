5602da212a75999fc45bcb5fd7d0e81c77b9e8a0
SlankehWeb <101549379+SlankehWeb@users.noreply.github.com>
2022-10-27 07:30:05 +0200

    arrow function version

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,66 +1,95 @@
-availableImages=['/assets/img/Betta.webp','/assets/img/Celestial-Pearl-Danio.webp','/assets/img/Discus.webp','/assets/img/Dwarf-Gourami.webp','/assets/img/Electric-Blue-Hap.webp','/assets/img/Electric-Yellow-Lab.webp',
-'/assets/img/Florida-Flag-Fish.webp','/assets/img/German-blue-ram.webp','/assets/img/Betta.webp','/assets/img/Celestial-Pearl-Danio.webp','/assets/img/Discus.webp','/assets/img/Dwarf-Gourami.webp','/assets/img/Electric-Blue-Hap.webp','/assets/img/Electric-Yellow-Lab.webp',
-'/assets/img/Florida-Flag-Fish.webp','/assets/img/German-blue-ram.webp']
+//grab things we need
+const section = document.querySelector("section");
 
-function start(){
-    let moves =0;
-    const maindiv = document.getElementById("boardgame");
-    while (maindiv.firstChild) {
-        maindiv.removeChild(maindiv.lastChild);
-    }
-    const scorediv = document.getElementById("score");
-    while (scorediv.firstChild) {
-       scorediv.removeChild(scorediv.lastChild);
-    }
 
-    var row = document.createElement('div')
-    ImagesCopy= JSON.parse(JSON.stringify( availableImages))
-    for(let j=1;j<=16;j++){
-        var div = document.createElement('div');
-        div.setAttribute('class','imgdiv')
-        var image = document.createElement('img')
-        randomImg = ImagesCopy.splice(Math.floor(Math.random() * ImagesCopy.length),1);
-        image.setAttribute('src',randomImg);
-        image.setAttribute('class','hide')
-        div.appendChild(image)
-        row.appendChild(div);
-        
-        if(j%4==0){
-            document.getElementById('boardgame').append(row)
-            row = document.createElement('div')
-        }
+//generate data 
+const getData = () => [
+    {imgSrc: "./assets/img/Betta.webp", name: "Betta"},
+    {imgSrc: "./assets/img/Celestial-Pearl-danio.webp", name: "Celestial Pearl danio"},
+    {imgSrc: "./assets/img/Discus.webp", name: "Discus"},
+    {imgSrc: "./assets/img/Dwarf-Gourami.webp", name: "Dwarf Gourami"},
+    {imgSrc: "./assets/img/Electric-Blue-Hap.webp", name: "Electric Blue Hap"},
+    {imgSrc: "./assets/img/Electric-Yellow-Lab.webp", name: "Electric Yellow Lab"},
+    {imgSrc: "./assets/img/Florida-Flag-Fish.webp", name: "Florida Flag Fish"},
+    {imgSrc: "./assets/img/German-blue-ram.webp", name: "German blue ram"},
+    {imgSrc: "./assets/img/Betta.webp", name: "Betta"},
+    {imgSrc: "./assets/img/Celestial-Pearl-danio.webp", name: "Celestial Pearl danio"},
+    {imgSrc: "./assets/img/Discus.webp", name: "Discus"},
+    {imgSrc: "./assets/img/Dwarf-Gourami.webp", name: "Dwarf Gourami"},
+    {imgSrc: "./assets/img/Electric-Blue-Hap.webp", name: "Electric Blue Hap"},
+    {imgSrc: "./assets/img/Electric-Yellow-Lab.webp", name: "Electric Yellow Lab"},
+    {imgSrc: "./assets/img/Florida-Flag-Fish.webp", name: "Florida Flag Fish"},
+    {imgSrc: "./assets/img/German-blue-ram.webp", name: "German blue ram"},
+];
 
-        div.addEventListener('click',function(event){
-            moves++;
-            let curr = event.currentTarget.children
-            let currImg = curr[0]
-           var currentlyshowing = document.getElementsByClassName('showimg');
-            currentlyshowing = document.getElementsByClassName('showimg');
-            let flag=0;
-            if(currentlyshowing.length >= 1){
-                for(let i=0;i<currentlyshowing.length;i++)
-                {
-                    if(currentlyshowing[i].src != currImg.src)
-                    currentlyshowing[i].classList.remove('showimg');
-                    else{
-                        currentlyshowing[i].classList.add('match');
-                        currImg.classList.add('match')
-                        flag=1;
-                    }
-                }
-            }
-
-            if(document.getElementsByClassName('match').length==16){
-                alert("you win")
-                let button = document.createElement('button');
-                button.setAttribute('class' , 'btn btn-warning');
-                let node= document.createTextNode("You win Moves "+moves);
-                button.appendChild(node)
-                document.getElementById('score').appendChild(button) 
-            }
-
-            if(flag==0)
-            currImg.classList.add('showimg');
-        })
-    }
+//randomize cards
+const randomize = () => {
+    const cardData = getData();
+    cardData.sort(() => Math.random() - 0.5);
+    return cardData;    
 }
+
+//card generator function
+const cardGenerator = () => {
+    const cardData = randomize();
+    // create html
+    cardData.forEach((item, index) => {
+        const card = document.createElement("div");
+        const face = document.createElement("img");
+        const back = document.createElement("div");
+        card.classList = "card";
+        face.classList = "face";
+        back.classList = "back";
+        // target data to the card
+        face.src = item.imgSrc;
+        card.setAttribute('name', item.name);
+        //target the cards to the section
+        section.appendChild(card);
+        card.appendChild(face);
+        card.appendChild(back);
+
+        card.addEventListener('click', (e) =>{
+            card.classList.toggle("toggleCard");
+            checkCards(e);
+        })
+    });
+};
+
+// check the cards
+const checkCards = (e) => {
+    console.log(e);
+    const clickedCard = e.target;
+    clickedCard.classList.add("flipped");
+    const flippedCards = document.querySelectorAll('.flipped');
+    if(flippedCards.length === 2){
+        if(
+            flippedCards[0].getAttribute("name") === 
+            flippedCards[1].getAttribute("name")
+    ) {
+        console.log("match");
+        flippedCards.forEach((card) => {
+            card.classList.remove("flipped");
+            card.style.pointerEvents = "none";
+        });
+    } else {
+        console.log("wrong");
+        flippedCards.forEach((card) => {
+            card.classList.remove("flipped");
+            setTimeout(() => card.classList.remove ("toggleCard"), 1000);
+        });
+    }
+  }
+};
+
+//restart
+// const restart = () => {
+//     let cardData = randomize();
+//     let faces = document.querySelectorAll(".face");
+//     let cards = document.querySelectorAll(".card");
+//     cardData.forEach((item,index) => {
+//         cards[index].classList.remove("toggleCard");
+//     })
+
+// }
+
+cardGenerator();
